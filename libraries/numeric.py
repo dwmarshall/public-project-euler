d93589ea89438ef90d31e13f52805994c280fcff
David Marshall <dmarshall@gmail.com>
2024-07-11 11:59:34 -0700

    problem 5

---
+++ libraries/numeric.py
@@ -0,0 +1,28 @@
+from typing import List
+
+def gcd(u: int, v: int) -> int:
+    multiplier = 1
+    while u % 2 == 0 and v % 2 == 0:
+        u //= 2
+        v //= 2
+        multiplier *= 2
+
+    t = -v if u % 2 == 1 else u
+
+    while t != 0:
+        while t % 2 == 0:
+            t //= 2
+        if t > 0:
+            u = t
+        else:
+            v = -t
+        t = u - v
+
+    return u * multiplier
+
+def lcm(n: List[int]) -> int:
+    if len(n) == 2:
+        return n[0] * n[1] // gcd(n[0], n[1])
+    else:
+        subLCM = lcm(n[1:])
+        return n[0] * subLCM // gcd(n[0], subLCM)
---
+++ p005.py
@@ -0,0 +1,3 @@
+from libraries import numeric
+
+print(numeric.lcm(list(range(1, 21))))
