dab944ed24eaf9effec952cfd0214abe35760082
David Marshall <dmarshall@gmail.com>
2024-07-12 15:54:10 -0700

    feat: problem 14
    Previously, I implemented my own @cache.

---
+++ p014.py
@@ -0,0 +1,22 @@
+from functools import cache
+
+@cache
+def collatz(n: int) -> int:
+    if n == 1:
+        return 1
+    if n % 2 == 1:
+        return 1 + collatz(3 * n + 1)
+    else:
+        return 1 + collatz(n // 2)
+
+maxTerms = 0
+maxN = 0
+
+N = 1000000
+
+for n in range(1, N):
+    terms = collatz(n)
+    if terms > maxTerms:
+        maxN, maxTerms = n, terms
+
+print(maxN)
