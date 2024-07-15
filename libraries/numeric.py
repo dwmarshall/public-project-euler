8d82027171b51286b3483a97214306cccef4614b
David Marshall <dmarshall@gmail.com>
2024-07-15 15:12:19 -0700

    feat: divisor related methods

--- libraries/numeric.py
+++ libraries/numeric.py
@@ -1,5 +1,13 @@
+from collections import Counter
 from generators import primes
+from functools import cache, reduce
+from itertools import product
+from math import sqrt
 from typing import List
+
+# Divisors stuff comes from this page:
+# https://rosettacode.org/wiki/Proper_divisors#Python
+
 
 def gcd(u: int, v: int) -> int:
     multiplier = 1
@@ -21,12 +29,14 @@ def gcd(u: int, v: int) -> int:
 
     return u * multiplier
 
+
 def lcm(n: List[int]) -> int:
     if len(n) == 2:
         return n[0] * n[1] // gcd(n[0], n[1])
     else:
         subLCM = lcm(n[1:])
         return n[0] * subLCM // gcd(n[0], subLCM)
+
 
 def number_of_divisors(n: int) -> int:
     divisors = 1
@@ -39,3 +49,46 @@ def number_of_divisors(n: int) -> int:
         if exponent:
             divisors *= exponent + 1
     return divisors
+
+
+@cache
+def prime_divisors(n: int) -> List[int]:
+    """
+    a list of the prime divisors, such as
+    220 => [2, 2, 5, 11]
+    """
+    for i in range(2, int(sqrt(n) + 1)):
+        d, m = divmod(n, i)
+        if not m:
+            return [i] + prime_divisors(d)
+    return [n]
+
+
+def prime_factors(n: int) -> dict:
+    """
+    A dictionary of the prime divisors of a number,
+    such as 220 => {2: 2, 5: 1, 11: 1}
+    """
+    d = prime_divisors(n)
+    # rosettacode.org has this line, not sure what the
+    # d[-1] == d is about, however
+    # d = [] if d == [n] else (d[:-1] if d[-1] == d else d)
+    d = [] if d == [n] else d
+    return dict(Counter(d))
+
+
+def proper_divisors(n: int) -> set:
+    """
+    The set of proper divisors of a number,
+    e.g. 220 => {1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110}
+    """
+    factors_dict = prime_factors(n)
+    print(factors_dict)
+    factors, occurrences = factors_dict.keys(), factors_dict.values()
+    multiplicities = product(*(range(oc + 1) for oc in occurrences))
+    divs = {
+        reduce(int.__mul__, (pf ** m for pf, m in zip(factors, multis)), 1)
+        for multis in multiplicities
+    }
+    divs.discard(n)
+    return divs or ({1} if n != 1 else set())
--- tests/libraries/test_numeric.py
+++ tests/libraries/test_numeric.py
@@ -1,11 +1,27 @@
 from libraries import numeric
 
+
 def test_gcd():
-    assert(numeric.gcd(874, 1406) == 38)
+    assert numeric.gcd(874, 1406) == 38
+
 
 def test_lcm():
-    assert(numeric.lcm([330, 225]) == 4950)
-    assert(numeric.lcm([330, 75, 450, 11, 37]) == 183150)
+    assert numeric.lcm([330, 225]) == 4950
+    assert numeric.lcm([330, 75, 450, 11, 37]) == 183150
+
 
 def test_number_of_divisors():
-    assert(numeric.number_of_divisors(220) == 12)
+    assert numeric.number_of_divisors(220) == 12
+
+
+def test_prime_divisors():
+    assert numeric.prime_divisors(37) == [37]
+    assert numeric.prime_divisors(220) == [2, 2, 5, 11]
+
+
+def test_prime_factors():
+    assert numeric.prime_factors(220) == {2: 2, 5: 1, 11: 1}
+
+def test_proper_divisors():
+    assert numeric.proper_divisors(220) == {1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110}
+    assert sum(numeric.proper_divisors(220)) == 284
