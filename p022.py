05c5d7629e4953ed1a91ca2034b79d6fc19dbcf6
David Marshall <dmarshall@gmail.com>
2024-07-15 15:53:24 -0700

    feat: problem 22

---
+++ p022.py
@@ -0,0 +1,12 @@
+f = open("data/names.txt")
+s = f.read()
+f.close()
+
+names = s.split(",")
+for i in range(len(names)):
+    names[i] = names[i][1:-1]
+names.sort()
+score = 0
+for i in range(len(names)):
+    score += (i + 1) * sum(ord(c) - ord("A") + 1 for c in names[i])
+print(score)
