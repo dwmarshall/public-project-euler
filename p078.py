b0927d72fa03c3e2834f1cf194983354112376d9
David Marshall <dmarshall@gmail.com>
2024-08-03 14:58:37 -0700

    refactor: improve problem 78

--- p078.py
+++ p078.py
@@ -1,5 +1,9 @@
 from functools import cache
+from generators.polygonal import pentagonal_numbers
 from itertools import count
+
+P = pentagonal_numbers(generalized=True)
+term_list = [next(P)]
 
 
 @cache
@@ -9,22 +13,26 @@ def partition(n: int) -> int:
     if n < 0:
         return 0
     w = 0
-    k = 1
-    while True:
-        pent1 = k * (3 * k - 1) // 2
-        pent2 = k * (3 * k + 1) // 2
-        if pent1 > n:
+    s = 1
+    for k in count():
+        if 2 * k + 1 >= len(term_list):
             break
-        w += (-1) ** (k - 1) * partition(n - pent1)
-        if pent2 <= n:
-            w += (-1) ** (k - 1) * partition(n - pent2)
-        k += 1
+        term1 = term_list[2 * k + 1]
+        if term1 > n:
+            break
+        w += s * partition(n - term1)
+        if 2 * k + 2 < len(term_list) and (term2 := term_list[2 * k + 2]) <= n:
+            w += s * partition(n - term2)
+        s *= -1
+
     return w
 
 
 D = 10**6
 
 for n in count(1):
+    while max(term_list) < n:
+        term_list.append(next(P))
     if partition(n) % D == 0:
         print(n, partition(n))
         break
