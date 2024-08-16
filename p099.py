0c05d6a82b0d6798f02180ca70a4a36685fbcb16
David Marshall <dmarshall@gmail.com>
2024-08-16 16:22:06 -0700

    feat: problem 99

---
+++ p099.py
@@ -0,0 +1,14 @@
+from math import log
+
+data = []
+
+with open("data/0099_base_exp.txt") as f:
+    for i, s in enumerate(f):
+        s = s.strip()
+        line_number, base, exponent = i, *s.split(",")
+        data.append((int(exponent) * log(int(base)), line_number))
+
+data.sort(reverse=True)
+
+# data[0][1] + 1 is the line with the largest number
+print(data[0][1] + 1)
